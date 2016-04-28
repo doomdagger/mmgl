ca7d56ba0aac8710e262be7e003681b180f8cf33
He Li <jet.in.brain@gmail.com>
2016-04-28 14:28:36 -0400

    update example according to new api

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -12,15 +12,16 @@ int main(int argc, char *argv[]) {
     if (argc == 2) {
     	scene.config.thread_num(stoi(argv[1]));
     }
-    scene.config.pixel_sampling_num(2);
-    scene.config.render_flag(Render::BVH);
+    scene.config.pixel_sampling_num(2).render_flag(Render::BVH);
+//    scene.config.render_flag(Render::BVH);
 
     Sphere &sphere = scene.sphere().at(0, 13.67f, -62.57f).radius(10);
 
     scene.pointLight(-80, 120, -46.6f, 1, 1, 1);
     scene.ambientLight(0.05, 0.05, 0.05);
 
-    scene.configCamera(24.2, 29.3, 53.6, 0, 0, -1, 35, 35, 35, 1000, 1000);
+//    scene.configCamera(24.2, 29.3, 53.6, 0, 0, -1, 35, 35, 35, 1000, 1000);
+    scene.camera().at(24.2, 29.3, 53.6).facing(0, 0, -1).focal_length(35).view_range(35, 35).image_size(1000, 1000);
 
     scene.render();
 

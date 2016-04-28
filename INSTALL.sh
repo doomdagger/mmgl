f4e2190e52b9c609e1d0f42fcfb0c5c861b74644
He Li <jet.in.brain@gmail.com>
2016-04-28 00:24:55 -0400

    update install.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # create and change to build directory
-mkdir build && cd build
+BUILD_DIR="${PWD}/build"
+
+if [ ! -d "$BUILD_DIR" ]; then
+  mkdir -m 755 "$BUILD_DIR"
+fi
+
+cd "$BUILD_DIR"
 
 # execute cmake, attention: two points here
 cmake ..
@@ -10,4 +16,4 @@ cmake ..
 make
 
 # install project, default installation path is /usr/local/include and /usr/local/lib
-make install
+sudo make install
